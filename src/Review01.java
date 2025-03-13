79023ea6fa5195042cfecc08bc13b6f1f6375cc6
Yuki-mmk <escarcha0707@icloud.com>
2025-03-13 22:36:23 +0900

    課題提出

---
+++ src/Review01.java
@@ -0,0 +1,16 @@
+
+public class Review01 {
+
+	public static void main(String[] args) {
+		int result1 =  tax(1500);
+		int num1 = 1500;
+		System.out.println((num1) +"円の商品の税込価格は" +(result1) +"円(消費税は" +(result1 - num1) +"円)です。");
+	}
+
+	public static int tax(int num1) {
+		int num2 = 10;
+		int result1 = num1 + num1 / 100 * num2;
+		return result1;
+	}
+
+}
