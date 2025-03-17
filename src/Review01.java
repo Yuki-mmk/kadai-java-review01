ba970cebf5530b56b51c65b7972040453cc95ec4
Yuki-mmk <escarcha0707@icloud.com>
2025-03-17 15:10:10 +0900

    再提出2

--- src/Review01.java
+++ src/Review01.java
@@ -2,14 +2,14 @@
 public class Review01 {
 
 	public static void main(String[] args) {
-		int result1 =  tax(1500);
 		int num1 = 1500;
+		int result1 =  tax(num1);
 		System.out.println((num1) +"円の商品の税込価格は" +(result1 + num1) +"円(消費税は" +(result1) +"円)です。");
 	}
 
 	public static int tax(int num1) {
 		int num2 = 10;
-		int result1 = num1 / 100 * num2;
+		int result1 = num1 * num2 / 100;
 		return result1;
 	}
 
